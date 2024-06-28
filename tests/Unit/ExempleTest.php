06c1d28485fd05f02bd02fd220e66232ed312988
1FarZ1 <fbekkouche149@gmail.com>
2024-06-28 16:50:31 +0100

    completed unit testing for auth

---
+++ tests/Unit/ExempleTest.php
@@ -0,0 +1,58 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+uses(TestCase::class);
+uses(RefreshDatabase::class);
+
+it('can register a user', function () {
+    $response = $this->postJson('/api/auth/register', [
+        'name' => 'John Doe',
+        'email' => 'john@example.com',
+        'password' => 'password',
+    ]);
+
+    $response->assertStatus(201);
+    $this->assertDatabaseHas('users', ['email' => 'john@example.com']);
+});
+
+it('can login a user', function () {
+    $user = User::factory()->create(['password' => bcrypt('password')]);
+
+    $response = $this->postJson('/api/auth/login', [
+        'email' => $user->email,
+        'password' => 'password',
+    ]);
+
+    $response->assertStatus(200);
+    $response->assertJsonStructure(['token']);
+});
+
+
+it('cannot login a user with invalid password', function () {
+    $user = User::factory()->create(['password' => bcrypt('password')]);
+
+    $response = $this->postJson('/api/auth/login', [
+        'email' => $user->email,
+        'password' => 'invalid-password',
+    ]);
+
+    $response->assertStatus(422);
+    $response->assertJsonValidationErrors('email');
+});
+
+it('cannot register a user with an existing email', function () {
+    $user = User::factory()->create();
+
+    $response = $this->postJson('/api/auth/register', [
+        'name' => 'John Doe',
+        'email' => $user->email,
+        'password' => 'password',
+
+    ]);
+
+    $response->assertStatus(422);
+    $response->assertJsonValidationErrors('email');
+});
